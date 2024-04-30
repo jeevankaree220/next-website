360216925fe79449dc2a6df58fbf23f7307c4e36
Aquo Digital <aquodigital@gmail.com>
2024-05-01 00:35:48 +0530

    Code to Sandbox ready4

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Hero from "../app/components/Hero"
+import Clients from "../app/components/Clients"
+
 export default function Home() {
   return (
     <>
       <Hero/>
+      <Clients/>
     </>
   );
 }
