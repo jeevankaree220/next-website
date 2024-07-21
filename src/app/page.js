a705537d234a7cf1cae783c8b0d2f428b3b93f66
Aquo Digital <aquodigital@gmail.com>
2024-07-21 20:38:26 +0530

    Code to Sandbox ready6

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import Hero from "./components/HeroComp";
 import Dashboard from "./components/Dashboard";
 import Features from "./components/Features";
 
+
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-white text-slate-800 px-4 lg:px-8">
