53d250eeb0aaf31b8870fed5ae5198c95d31f9ae
Jeevan Kumar <99116369+jeevankaree220@users.noreply.github.com>
2024-07-21 15:06:34 +0000

    New changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,10 @@
 import Image from "next/image";
-import Header from "./components/Header";
-import Hero from "./components/HeroComp";
-import Dashboard from "./components/Dashboard";
-import Features from "./components/Features";
+import Landingpage from "../app/components/Landingpage";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white text-slate-800 px-4 lg:px-8">
-      <Header />
-      <main className="">
-        <Hero />
-        <Features />
-      </main>
-    </div>
+    <>
+      <Landingpage />
+    </>
   );
 }
